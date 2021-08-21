5a1af630ac977b33bbe3652ad11a1b524d2c6cf3
melq <tu2sice@yahoo.co.jp>
2021-08-21 16:14:30 +0900

    :sparkles: マウスクリックでランダムにボールが飛び出すようにした

--- src/gameboard/PinBall.kt
+++ src/gameboard/PinBall.kt
@@ -6,9 +6,12 @@ import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.Timer
+import kotlin.random.Random
 
 class PinBall {
     fun startGame() {
@@ -25,13 +28,14 @@ class PinBall {
     }
 }
 
-class GameBoard : JPanel(), ActionListener {
+class GameBoard : JPanel(), ActionListener, MouseListener {
     private val timer = Timer(16, this)
     private val drawableList = mutableListOf<Drawable>()
     private val ballFactory = BallFactory(drawableList)
 
     fun start() {
         timer.start()
+        addMouseListener(this)
         ballFactory.create(this)
     }
 
@@ -47,4 +51,23 @@ class GameBoard : JPanel(), ActionListener {
             drawable.next()
         repaint()
     }
+
+    override fun mouseClicked(e: MouseEvent?) { }
+
+    override fun mousePressed(e: MouseEvent?) {
+        e!!
+        val x = e.x
+        val y = e.y
+
+        if (e.button == MouseEvent.BUTTON1) {
+            println("random!")
+            ballFactory.randomBalls(x = x.toDouble(), y = y.toDouble(), panel = this)
+        }
+    }
+
+    override fun mouseReleased(e: MouseEvent?) { }
+
+    override fun mouseEntered(e: MouseEvent?) { }
+
+    override fun mouseExited(e: MouseEvent?) { }
 }
--- src/gameboard/drawable/BallFactory.kt
+++ src/gameboard/drawable/BallFactory.kt
@@ -1,13 +1,40 @@
 package gameboard.drawable
 
+import java.awt.Color
 import javax.swing.JPanel
+import kotlin.random.Random
 
 class BallFactory(private val drawableList: MutableList<Drawable>) : Factory() {
+    private val random = Random(1)
+
     override fun createDrawable(panel: JPanel): Drawable {
-        return Ball(panel = panel)
+        return Ball(
+            panel = panel,
+            color = Color(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256)))
     }
 
     override fun registerDrawable(drawable: Drawable) {
         drawableList.add(drawable)
+    }
+
+    fun randomBalls(r: Double = 10.0,
+                    x: Double = 0.0,
+                    y: Double = 0.0,
+                    panel: JPanel,
+                    n: Int = 10) {
+        for (i in 0 .. n) {
+            registerDrawable(
+                Ball(r, x, y,
+                    (random.nextInt(30) - 15).toDouble(),
+                    (random.nextInt(15) - 15).toDouble(),
+                    Color(
+                        random.nextInt(256),
+                        random.nextInt(256),
+                        random.nextInt(256)),
+                    panel))
+        }
     }
 }
